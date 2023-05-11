e417c4f3e8cc55c619a2664cda6ca842fe6502d4
Benard Mosomi <benardmosomi@gmail.com>
2023-05-11 14:21:05 +0300

    sba

--- roberms/settings.py
+++ roberms/settings.py
@@ -89,13 +89,13 @@ WSGI_APPLICATION = 'roberms.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-DATABASES = {
+DATABASES1 = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES1 = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sba_water',
