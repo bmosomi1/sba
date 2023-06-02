9d3b351683287f17eca2b5d729e0fd22600a9d28
Benard Mosomi <benardmosomi@gmail.com>
2023-06-02 17:00:25 +0300

    orders

--- mpesa_api/models.py
+++ mpesa_api/models.py
@@ -5,6 +5,20 @@ from django.utils import timezone
 
 from sms.models import Customer
 
+class TeuleMpesa(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    description = models.TextField()
+    phone_number = models.CharField(max_length=250)
+    amount = models.CharField(max_length=250)
+    reference = models.CharField(max_length=250)
+    email = models.CharField(max_length=250)
+    type = models.CharField(max_length=250)
+    account_number = models.IntegerField(null=True)
+    till_number = models.IntegerField(null=True)
+    processed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(null=True)
+    organization_balance = models.CharField(max_length=250, null=True)
 class RobermsMpesa(models.Model):
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
